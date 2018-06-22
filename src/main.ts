efffd99d4bb52bbf5b52337f3bb3210afb3d533a
Emerson Luiz <eluizbr@gmail.com>
2018-06-22 09:28:53 -0300

    Angular meetup

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+declare var requirejs: any;
+
 if (environment.production) {
   enableProdMode();
 }
+
+requirejs.config({
+  baseUrl: '.'
+});
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
