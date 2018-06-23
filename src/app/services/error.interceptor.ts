6ea296281e951c5a0d6a76d6e5a53162729a218c
Emerson Luiz <eluizbr@MacBook-Pro-de-Emerson.local>
2018-06-23 16:36:32 -0300

    Angular meetup

--- src/app/services/error.interceptor.ts
+++ src/app/services/error.interceptor.ts
@@ -1,16 +1,24 @@
 import { HttpErrorResponse, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 /** Pass untouched request through to the next request handler. */
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {}
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         console.log(error.error);
         alert(error.error);
+        switch (error.status) {
+          case 401:
+            localStorage.clear();
+            this.router.navigate(['/login']);
+            break;
+        }
         return throwError(error);
       })
     );
